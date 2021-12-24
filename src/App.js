7e18c48d9225eff732faa036435c5b9a661a865f
HectorSaldes <20193tn070@utez.edu.mx>
2021-12-23 19:35:57 -0600

    Changing Browser to Hash router

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from './pages/Home'
 import Icons from './pages/Icons'
 import Images from './pages/Images'
 import Error from './pages/Error'
-import {BrowserRouter as MainBrowser, Route, Routes} from 'react-router-dom'
+import {HashRouter as MainBrowser, Route, Routes} from 'react-router-dom'
 
 export default function App() {
     return (
