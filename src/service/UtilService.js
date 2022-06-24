a1bb4384ffea0cda1eaa1e754cb4fe3a5996d205
hectorsaldes <=>
2022-06-24 09:21:16 -0500

    Add lines to execute PWA

--- src/service/UtilService.js
+++ src/service/UtilService.js
@@ -1,12 +1,1 @@
-
 export const handleScroll = () => window.pageYOffset;
-
-export const messages = (severity, summary, detail) => {
-  // eslint-disable-next-line no-undef
-  toast.current.show({
-    severity,
-    summary,
-    detail,
-    life: 3000,
-  });
-};
