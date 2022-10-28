7ceabefc3f6df9542e5730038883fe1a51db963b
HectorSaldes <20193tn070@utez.edu.mx>
2022-10-27 20:59:51 -0500

    Fix deploy

--- src/service/IllustrationsService.js
+++ src/service/IllustrationsService.js
@@ -10,6 +10,7 @@ class IllustrationsService {
 
   searchIllustrations(query, page = 1, category = "") {
     return axios
+      // eslint-disable-next-line no-template-curly-in-string
       .get(`${CORS_ENDPONIT}https://api-ouch.icons8.com/api/frontend/v1/illustrations?locale=en-US&page=${page}&per_page=50&search=${query}${category.length === 0 ? "" : "&style_pretty_ids=${category}"}`)
       .then(({ data: { illustrations } }) => illustrations)
       .catch(() => null);
