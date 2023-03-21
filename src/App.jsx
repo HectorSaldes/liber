1f29f115dde1e507ed197f5303e43f75e135502a
HectorSaldes <20193tn070@utez.edu.mx>
2023-03-20 20:19:43 -0600

    LIB-6 add blog cors

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Menu from "./components/Menu";
 import Home from "./pages/Home";
 import IconsByWord from "./pages/icons/IconsByWord";
 import Images from "./pages/Images";
-import Illustrations from "./pages/Illustrations";
 // rome-ignore lint/suspicious/noShadowRestrictedNames: <explanation>
 import  Error from "./pages/Error";
 import IconsById from "./pages/icons/IconsById";
@@ -18,7 +17,6 @@ export default function App() {
 				<Route path='/icons' element={<IconsByWord />} />
 				<Route path='/icons/by-id' element={<IconsById />} />
 				<Route path='/images' element={<Images />} />
-				<Route path='/illustrations' element={<Illustrations />} />
 				<Route path='*' element={<Error />} />
 			</Routes>
 		</MainBrowser>
